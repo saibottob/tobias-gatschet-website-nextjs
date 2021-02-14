ac10317864ac40a688a116fc9d323101719439e9
Tobias Gatschet <saibottob.gatschet@gmail.com>
2021-02-14 01:10:48 +0100

    style and make footer

--- components/layout.tsx
+++ components/layout.tsx
@@ -7,13 +7,16 @@ import {faFacebook, faInstagram, faTwitter, faLinkedin} from '@fortawesome/free-
 import {faEnvelope} from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 
+// Style
+import styles from './../styles/Layout.module.scss'
+
 type Props = {
     children?: ReactNode
     title?: string
 }
 
-const Layout = ({ children, title = 'This is the default title' }: Props) => (
-    <div>
+const Layout = ({ children, title = 'Tobias Gatschet' }: Props) => (
+    <div className={styles['main-wrapper']}>
         <Head>
             <title>{title}</title>
             <meta charSet="utf-8" />
@@ -34,16 +37,15 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
 
         {children}
 
-        <footer>
-            <div>
+        <footer className={styles.footer}> 
+            <div className={styles.footer_social_media}>
 				<a target="_blank" href="https://www.facebook.com/tobias.gatschet"><FontAwesomeIcon icon={faFacebook} /></a>
 				<a target="_blank" href="https://twitter.com/saibottob"><FontAwesomeIcon icon={faTwitter} /></a>
 				<a target="_blank" href="https://www.linkedin.com/in/tobias-gatschet-396441198/"><FontAwesomeIcon icon={faLinkedin} /></a>
 				<a target="_blank" href="https://www.instagram.com/ramziagob/"><FontAwesomeIcon icon={faInstagram} /></a>
 				<a target="_blank" href="mailto:gatschet@tobias-gatschet.ch"><FontAwesomeIcon icon={faEnvelope} /></a>
 			</div>
-
-			<div>&copy; Tobias Gatschet 2021</div>
+			<div className={styles.footer_personal_info}>&copy; Tobias Gatschet 2021</div>
         </footer>
     </div>
 )
